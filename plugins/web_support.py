d188557745058d8e345baf8f3a297f7ea7b9b7c1
Tony Stark <138078424+TonyStarkBotz@users.noreply.github.com>
2024-07-11 12:32:24 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -5,9 +5,9 @@
 
 from aiohttp import web
 
-tony_FileRenameBot = web.RouteTableDef()
+Quick_FileRenameBot = web.RouteTableDef()
 
-@tony_FileRenameBot.get("/", allow_head=True)
+@Quick_FileRenameBot.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("tonystarkbotz")
 
@@ -15,6 +15,9 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(Quick_FileRenameBot)
     return web_app
+
+if __name__ == "__main__":
+    web.run_app(web_server())
 
 # Don't Remove Credit 😔
 # Telegram Channel @TonyStark_Botz & @MovieTimesTV
